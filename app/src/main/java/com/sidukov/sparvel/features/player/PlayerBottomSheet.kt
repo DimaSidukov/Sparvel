3f2ee5f9e5444828fc55d07254af819859de6323
DimaSidukov <dmitriy.sidukov@gmail.com>
2023-03-25 18:36:31 +0300

    draggable to swipeable

--- app/src/main/java/com/sidukov/sparvel/features/player/PlayerBottomSheet.kt
+++ app/src/main/java/com/sidukov/sparvel/features/player/PlayerBottomSheet.kt
@@ -1,22 +1,32 @@
 package com.sidukov.sparvel.features.player
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.core.*
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
 import androidx.compose.animation.graphics.res.animatedVectorResource
 import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ResistanceConfig
+import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material.swipeable
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
@@ -26,6 +36,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sidukov.sparvel.R
+import com.sidukov.sparvel.core.functionality.apiGreaterThan
 import com.sidukov.sparvel.core.functionality.background
 import com.sidukov.sparvel.core.functionality.normalize
 import com.sidukov.sparvel.core.model.Track
@@ -35,7 +46,16 @@ import com.sidukov.sparvel.core.widgets.SparvelImage
 import com.sidukov.sparvel.core.widgets.Toolbar
 import com.sidukov.sparvel.features.home.HomeViewModel
 import com.sidukov.sparvel.features.home.PlayerState
+import kotlinx.coroutines.launch
 
+private const val ANDROID_12 = android.os.Build.VERSION_CODES.S
+private const val animationDuration = 300
+
+enum class SwipeState {
+    COLLAPSED, EXPANDED
+}
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PlayerBottomSheet(
     viewModel: HomeViewModel,
@@ -47,51 +67,150 @@ fun PlayerBottomSheet(
                 WindowInsets.systemBars.getTop(LocalDensity.current).dp
     val minHeight = 150.dp
 
-    var shouldMoveDown by remember { mutableStateOf(false) }
     var playbackTimestamp by remember { mutableStateOf(0.3f) }
+    val swipeState = rememberSwipeableState(initialValue = SwipeState.COLLAPSED)
+    val scope = rememberCoroutineScope()
 
-    BackHandler {
-        shouldMoveDown = true
+    val gradientAnimationDuration = 20000
+    val gradientStartX = with(LocalDensity.current) {
+        LocalConfiguration.current.screenWidthDp.dp.toPx() / 1.4f
     }
+    val gradientStartY = with(LocalDensity.current) {
+        LocalConfiguration.current.screenHeightDp.dp.toPx() / 1.3f
+    }
+    val gradientX = if (apiGreaterThan(ANDROID_12)) rememberInfiniteTransition().animateFloat(
+        initialValue = gradientStartX - 300f,
+        targetValue = gradientStartX - 300f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = gradientAnimationDuration
+                gradientStartX - 300f at 0 with FastOutSlowInEasing
+                gradientStartX - 500f at gradientAnimationDuration / 4 with FastOutSlowInEasing
+                gradientStartX + 100f at gradientAnimationDuration / 3 with FastOutSlowInEasing
+                gradientStartX - 500f at gradientAnimationDuration / 2 with FastOutSlowInEasing
+                gradientStartX at gradientAnimationDuration with FastOutSlowInEasing
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    ).value else 0f
+    val gradientY = if (apiGreaterThan(ANDROID_12)) rememberInfiniteTransition().animateFloat(
+        initialValue = gradientStartY + 300f,
+        targetValue = gradientStartY + 300f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = gradientAnimationDuration
+                gradientStartY + 300f at 0 with FastOutSlowInEasing
+                gradientStartY + 500f at gradientAnimationDuration / 4 with FastOutSlowInEasing
+                gradientStartY + 400f at gradientAnimationDuration / 3 with FastOutSlowInEasing
+                gradientStartY - 100f at gradientAnimationDuration / 2 with FastOutSlowInEasing
+                gradientStartY + 300f at gradientAnimationDuration with FastOutSlowInEasing
+            }
+        )
+    ).value else 0f
 
-    DraggableBottomSheet(
-        screenHeight = screenHeight,
-        minHeight = minHeight,
-        shouldMoveDown = shouldMoveDown
-    ) { currentHeight, isLayoutExpanded ->
-        val expandedRatio = 0.2f
-        val collapsedRatio = 0.4f
-        val heightNormalized = currentHeight.value.normalize(screenHeight.value, minHeight.value)
-        val expandedAlpha =
-            if (heightNormalized < expandedRatio) 0f else heightNormalized.normalize(
-                1f,
-                expandedRatio
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(y = swipeState.offset.value.dp)
+                .align(Alignment.BottomCenter)
+                .shadow(20.dp, clip = true, shape = RoundedCornerShape(5, 5))
+                .clip(
+                    RoundedCornerShape(5, 5)
+                )
+                .swipeable(
+                    state = swipeState,
+                    orientation = Orientation.Vertical,
+                    resistance = ResistanceConfig(0f, 0f, 0f),
+                    anchors = mapOf(
+                        screenHeight.value - minHeight.value to SwipeState.COLLAPSED,
+                        0f to SwipeState.EXPANDED
+                    )
+                ),
+        ) {
+            val collapsedRatio = 0.6f
+            val expandedRatio = 0.8f
+            val heightNormalized by remember {
+                derivedStateOf {
+                    swipeState.offset.value.normalize(screenHeight.value - minHeight.value, 0f)
+                }
+            }
+            val collapsedAlpha by remember {
+                derivedStateOf {
+                    if (heightNormalized < collapsedRatio) 0f else heightNormalized.normalize(
+                        1f,
+                        collapsedRatio
+                    )
+                }
+            }
+            val expandedAlpha by remember {
+                derivedStateOf {
+                    if (heightNormalized > expandedRatio) 0f else heightNormalized.normalize(
+                        0f,
+                        expandedRatio
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(SparvelTheme.colors.background)
+                    .blur(150.dp)
+                    .background(
+                        if (apiGreaterThan(ANDROID_12)) Brush.sweepGradient(
+                            SparvelTheme.colors.playerBackground,
+                            Offset(gradientX, gradientY)
+                        ) else {
+                            Brush.verticalGradient(
+                                SparvelTheme.colors.playerBackground
+                            )
+                        }
+                    )
             )
-        val collapsedAlpha =
-            if (heightNormalized > collapsedRatio) 0f else heightNormalized.normalize(
-                0f,
-                collapsedRatio
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(0.7f)
+                    .clickable(
+                        enabled = swipeState.currentValue == SwipeState.COLLAPSED,
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() },
+                        onClick = {
+                            scope.launch {
+                                swipeState.animateTo(
+                                    SwipeState.EXPANDED, tween(animationDuration)
+                                )
+                            }
+                        }
+                    )
             )
-
-        SideEffect {
-            if (isLayoutExpanded) {
-                shouldMoveDown = false
+            CollapsedPlayer(
+                viewModel = viewModel,
+                alpha = collapsedAlpha,
+                height = minHeight / 2,
+                track = viewModel.uiState.selectedTrack!!,
+            )
+            ExpandedPlayer(
+                viewModel = viewModel,
+                alpha = expandedAlpha,
+                playbackTimestamp = playbackTimestamp,
+                iconColor = iconColor,
+                onCollapseClicked = {
+                    scope.launch {
+                        swipeState.animateTo(SwipeState.COLLAPSED, tween(animationDuration))
+                    }
+                },
+                onSliderValueChanged = { playbackTimestamp = it },
+            )
+        }
+    }
+    if (swipeState.currentValue == SwipeState.EXPANDED) {
+        BackHandler {
+            scope.launch {
+                swipeState.animateTo(SwipeState.COLLAPSED)
             }
         }
-        CollapsedPlayer(
-            viewModel = viewModel,
-            alpha = collapsedAlpha,
-            height = minHeight / 2,
-            track = viewModel.uiState.selectedTrack!!,
-        )
-        ExpandedPlayer(
-            viewModel = viewModel,
-            alpha = expandedAlpha,
-            playbackTimestamp = playbackTimestamp,
-            iconColor = iconColor,
-            onCollapseClicked = { shouldMoveDown = true },
-            onSliderValueChanged = { playbackTimestamp = it },
-        )
     }
 }
 
